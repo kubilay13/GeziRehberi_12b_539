d050a28ecb8caa50a111fae68c7ff58111dc1245
Öğrenci <ogrenci@profiloatl.meb.k12.tr>
2024-03-21 11:21:45 +0300

    update

--- app/src/main/java/com/example/gezirehberi_12b_539/FragmentTrBilgi.java
+++ app/src/main/java/com/example/gezirehberi_12b_539/FragmentTrBilgi.java
@@ -3,8 +3,20 @@ package com.example.gezirehberi_12b_539;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import com.example.gezirehberi_12b_539.Activity_Kayit;
+import com.example.gezirehberi_12b_539.R;
+import com.example.gezirehberi_12b_539.databinding.ActivityFragmentTrBilgiBinding;
 
 public class FragmentTrBilgi extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
